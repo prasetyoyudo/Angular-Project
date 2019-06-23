bae88f5b9ca19656074e260d2604166f75b3fda9
prasetyoyudo <pyudo28@gmail.com>
2019-06-23 10:48:18 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContentComponent } from './content/content.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SocialComponent } from './social/social.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ConfigService } from './config.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    GalleryComponent,
+    ContentComponent,
+    TestimonialsComponent,
+    ClientsComponent,
+    PricingComponent,
+    FooterComponent,
+    HeaderComponent,
+    SocialComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ConfigService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/configuration.ts
@@ -0,0 +1,91 @@
+export const configuration = {
+    header: {
+        heading: "My Website",
+        headingtext: "Namari is a free landing page template you can use for your projects. It is free to use for your personal and commercial projects, enjoy!",
+        buttontext: "do some action !",
+        buttonlink: "\home"
+    },
+    intro: {
+        tagline: 'SUCCESS',
+        title: 'How We Help You To Sell Your Product',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!',
+        features: [
+            { icon: 'html5 ', title: 'HTML5 & CSS3', description: 'Has ne tritani atomorum conclusionemque, in dolorum volumus cotidieque eum. At vis choro neglegentur iudico' },
+            { icon: 'bolt', title: 'Easy to Use', description: 'Cu vero ipsum vim, doctus facilisi sea in. Eam ex falli honestatis repudiandae, sit detracto mediocrem disputationi' },
+            { icon: 'tablet', title: 'Fully Responsive', description: 'Id porro tritani recusabo usu, eum intellegam consequuntur et. Fugit debet ea sit, an pro nemore vivendum' },
+            { icon: 'rocket', title: 'Parallax Effect', description: 'Id porro tritani recusabo usu, eum intellegam consequuntur et. Fugit debet ea sit, an pro nemore vivendum' }
+        ]
+    },
+    services: {
+        tagline: 'BELIEVING',
+        title: 'Focusing On What Matters Most',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!'
+    },
+    testimonials: {
+        tagline: 'FEEDBACK',
+        title: 'What our customers are saying',
+        description: '',
+        feedbacks: [
+            { name: 'John Doe', userImage: 'user-1.jpg', comments: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', company: 'Happy Customer' },
+            { name: 'Roslyn Doe', userImage: 'user-2.jpg', comments: 'Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt utlabore et dolore magna aliqua', company: 'Happy Customer' },
+            { name: 'Thomas Doe', userImage: 'user-3.jpg', comments: 'Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', company: 'Happy Customer' }
+        ]
+    },
+    clients: {
+        tagline: 'TRUST',
+        title: 'Companies who use our services',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!',
+        companies: [
+            { name: 'Tree', weblink: 'company-logo1.png', logo: 'company-logo1.png' },
+            { name: 'Fingerprint', weblink: 'company-logo2.png', logo: 'company-logo2.png' },
+            { name: 'The Man', weblink: 'company-logo3.png', logo: 'company-logo3.png' },
+            { name: 'Mustache', weblink: 'company-logo4.png', logo: 'company-logo4.png' },
+            { name: 'Goat', weblink: 'company-logo5.png', logo: 'company-logo5.png' },
+            { name: 'Justice', weblink: 'company-logo6.png', logo: 'company-logo6.png' },
+            { name: 'Ball', weblink: 'company-logo7.png', logo: 'company-logo7.png' },
+            { name: 'Cold', weblink: 'company-logo8.png', logo: 'company-logo8.png' },
+            { name: 'Cold', weblink: 'company-logo9.png', logo: 'company-logo9.png' },
+        ]
+    },
+    pricing: {
+        tagline: 'YOUR CHOICE',
+        title: 'We have the right package for you',
+        description: '',
+        plans: [
+            {
+                name: 'PERSONAL', subtitle: 'The standard version', currency: 'Rp', price: '19', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', download: '5 Downloads', extension: '2 Extensions', tutorial: 'Tutorials', support: 'Forum Support', update: '1 year free updates',
+                buttontext: 'Buy Now', buttonlink: '#', featured: true
+            },
+            {
+                name: 'STUDENT', subtitle: 'Most popular choice', currency: 'Rp', price: '29', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', download: '15 Downloads', extension: '5 Extensions', tutorial: 'Tutorials with Files', support: 'Forum Support', update: '2 years free updates',
+                buttontext: 'Buy Now', buttonlink: '#', featured: true
+            },
+            {
+                name: 'BUSINESS', subtitle: 'For the whole team', currency: 'Rp', price: '49', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', download: 'Unlimited Downloads', extension: 'Unlimited Extensions', tutorial: 'HD Video Tutorials', support: 'Chat Support', update: 'Lifetime free updates',
+                buttontext: 'Buy Now', buttonlink: '#', featured: true
+            },
+        ]
+    },
+    gallery: {
+        images: [
+            'gallery-image-1.jpg',
+            'gallery-image-2.jpg',
+            'gallery-image-3.jpg',
+            'gallery-image-4.jpg',
+            'gallery-image-5.jpg',
+            'gallery-image-6.jpg'
+        ]
+    },
+    footer: {
+        copyrightText: 'Made with love by ',
+        developer: 'AdMISTER STUDIO',
+        developerLink: 'http://admister.in'
+    },
+    socials: [
+        { link: 'https://www.facebook.com/', target: '_blank', title: 'Facebook', username: 'username', icon: 'facebook' },
+        { link: 'http://google.com/', target: '_blank', title: 'Google', username: 'username', icon: 'google-plus' },
+        { link: 'http://www.twitter.com/', target: '_blank', title: 'Twitter', username: 'username', icon: 'twitter' },
+        { link: 'http://www.instagram.com/', target: '_blank', title: 'Instagram', username: 'username', icon: 'instagram' },
+        { link: 'http://www.behance.net/', target: '_blank', title: 'behance', username: 'username', icon: 'behance' },
+    ]
+}
